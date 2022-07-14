ada604c0679b1cdd3fdbf69bf11e5682e57ff6c6
Rodney Stanley <rodney_stanley@hotmail.com>
2022-07-14 16:47:21 -0500

    Added very simple code for divide

--- calculator.py
+++ calculator.py
@@ -13,3 +13,11 @@ def subtract(first_term, second_term):
 
 def multiply(first_term, second_term):
     return first_term * second_term
+
+
+def divide(first_term, second_term):
+    if second_term > first_term:
+        print('Cannot divide by zero')
+        return None
+    else:
+        return first_term / second_term
